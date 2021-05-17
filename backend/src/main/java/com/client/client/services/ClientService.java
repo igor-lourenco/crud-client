2c03ef1ca2e6820d18e6948c7ff201eccf10d2a3
igor-lourenco <igor.lourencosantos@gmail.com>
2021-05-17 16:05:11 -0300

    List Paged

--- backend/src/main/java/com/client/client/services/ClientService.java
+++ backend/src/main/java/com/client/client/services/ClientService.java
@@ -1,14 +1,14 @@
 package com.client.client.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,9 +24,9 @@ public class ClientService { // CAMADA SERVIÇO
 	private ClientRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<ClientDTO> findAll() {
-		List<Client> list = repository.findAll();
-		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
 	}
 
 	@Transactional(readOnly = true)
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -3,3 +3,27 @@ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Cristia
 INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Lionel Messi', 96587412036, 1000.0, TIMESTAMP WITH TIME ZONE '1980-05-20T20:59:19Z', 1);
 INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Neymar Junior', 79653015478, 3000.0, TIMESTAMP WITH TIME ZONE '1975-05-20T20:59:19Z', 2);
 INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Karin Benzema', 96663201554, 5000.0, TIMESTAMP WITH TIME ZONE '1989-05-20T20:59:19Z', 4);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Sergio Aguero', 17796533015, 3000.0, TIMESTAMP WITH TIME ZONE '1996-05-10T20:59:19Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Kevin de Bruyne', 63547812014, 2000.0, TIMESTAMP WITH TIME ZONE '1983-04-04T20:59:19Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Riyad Mahrez', 68745102412, 1000.0, TIMESTAMP WITH TIME ZONE '1980-05-20T20:59:19Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Phill Foden', 96325874102, 3000.0, TIMESTAMP WITH TIME ZONE '1975-05-20T20:59:19Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ferran Torres', 41025896375, 5000.0, TIMESTAMP WITH TIME ZONE '1989-05-20T20:59:19Z', 4);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Rubens Dias', 23214506987, 3000.0, TIMESTAMP WITH TIME ZONE '1996-05-10T20:59:19Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Raheen Sterling', 12345876985, 2000.0, TIMESTAMP WITH TIME ZONE '1983-04-04T20:59:19Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Gabriel Jesus', 24683519705, 1000.0, TIMESTAMP WITH TIME ZONE '1980-05-20T20:59:19Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ederson Santana', 40028922012, 3000.0, TIMESTAMP WITH TIME ZONE '1975-05-20T20:59:19Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Bernardo Silva', 75315982640, 5000.0, TIMESTAMP WITH TIME ZONE '1989-05-20T20:59:19Z', 4);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Nathan Aké', 64285319750, 3000.0, TIMESTAMP WITH TIME ZONE '1996-05-10T20:59:19Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('João Cancelo', 85461320975, 2000.0, TIMESTAMP WITH TIME ZONE '1983-04-04T20:59:19Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('John Stones', 12054986357, 1000.0, TIMESTAMP WITH TIME ZONE '1980-05-20T20:59:19Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Benjamin Mendy', 75463201865, 3000.0, TIMESTAMP WITH TIME ZONE '1975-05-20T20:59:19Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Eric Garcia', 75412036985, 5000.0, TIMESTAMP WITH TIME ZONE '1989-05-20T20:59:19Z', 4);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Zack Steffen', 36521478905, 3000.0, TIMESTAMP WITH TIME ZONE '1996-05-10T20:59:19Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Scott Carson', 02235416987, 2000.0, TIMESTAMP WITH TIME ZONE '1983-04-04T20:59:19Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Kyle Walker', 45456932582, 1000.0, TIMESTAMP WITH TIME ZONE '1980-05-20T20:59:19Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Liam Delap', 75496830210, 3000.0, TIMESTAMP WITH TIME ZONE '1975-05-20T20:59:19Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Claudio Gomes', 54682319750, 5000.0, TIMESTAMP WITH TIME ZONE '1989-05-20T20:59:19Z', 4);
